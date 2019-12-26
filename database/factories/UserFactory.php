665d425bb40a5bb23b358d9ae3e8aac5eb32d368
KatiaBen <44305117+KatiaBen@users.noreply.github.com>
2019-12-26 19:00:26 +0100

    Ajout de methode de remplissage de présence ramdom

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,8 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\User;
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,4 +24,18 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
+});
+
+$factory->define(App\Presences::class, function (Faker $faker){
+    $d=now();//$faker->numberBetween($min = 8, $max = 17);
+    return[ 
+        'Nom' => $faker->lastName(),
+        'Prenom' => $faker->firstName($gender = 'male'|'female'),
+        'Bureau' => $faker->numberBetween($min = 1, $max = 50),
+        'Date' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'Debut' => $d,
+        'Fin' => $d//$faker->numberBetween($min = $d, $max = 17)
+
+    ];
+
 });
