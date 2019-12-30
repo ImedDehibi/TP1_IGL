574467a4eec5f3e3dc0cc43d12ad836392e25413
akram <a.thabet@esi-sba.dz>
2019-12-30 02:57:35 +0100

    Our project merged with rest api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,31 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/EspaceAdministrateur/Enseignant','enseignantController@index');
+Route::get('/EspaceAdministrateur/Enseignant/{id}','enseignantController@show');
+Route::post('/EspaceAdministrateur/Enseignant','enseignantController@store');
+Route::put('/EspaceAdministrateur/Enseignant/{id}','enseignantController@update');
+Route::delete('/EspaceAdministrateur/Enseignant/{id}','enseignantController@destroy');
+
+
+
+Route::get('/EspaceAdministrateur/Etudiant','etudiantController@index');
+Route::get('/EspaceAdministrateur/Etudiant/{id}','etudiantController@show');
+Route::post('/EspaceAdministrateur/Etudiant','etudiantController@store');
+Route::put('/EspaceAdministrateur/Etudiant/{id}','etudiantController@update');
+Route::delete('/EspaceAdministrateur/Etudiant/{id}','etudiantController@destroy');
+
+
+
+Route::get('/EspaceEnseignant/Cours','CoursController@index');
+Route::post('/EspaceEnseignant/Cours','CoursController@store');
+
+
+
+Route::get('/EspaceEtudiant','Controller@ConsulterCours');
+Route::get('/EspaceEtudiant/file/{id}','Controller@downloadfile');
+
+
+
+
